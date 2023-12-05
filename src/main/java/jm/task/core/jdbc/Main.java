16cbf51f193dbe9cda5774559eeacdaf74d8c22d
BegginerHi1 <146478622+BegginerHi1@users.noreply.github.com>
2023-12-05 12:12:04 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,9 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
@@ -12,33 +11,36 @@ import java.util.List;
 
 public class Main {
 
-    private static UserDao userDao = new UserDaoJDBCImpl();
+    private static UserService userService = new UserServiceImpl();
 
     public static void main(String[] args) {
 
 
-        userDao.createUsersTable();
+        userService.createUsersTable();
 
 
-        List<User> userList = List.of(new User("Bob","Singer",(byte)25),
-                new User("Kate","White",(byte)30),
-                new User("Victory","Chance",(byte)35),
-                new User("Tom","Pitt",(byte)20));
+        List<User> userList = List.of(new User("Bob", "Singer", (byte) 25),
+                new User("Kate", "White", (byte) 30),
+                new User("Victory", "Chance", (byte) 35),
+                new User("Tom", "Pitt", (byte) 20));
         for (User user : userList) {
-            userDao.saveUser(user.getName(),user.getLastName(),user.getAge());
-            System.out.println("User c именем " +user.getName()+" добавлен в базу данных");
+            userService.saveUser(user.getName(), user.getLastName(), user.getAge());
+            System.out.println("User c именем " + user.getName() + " добавлен в базу данных");
         }
 
 
-        List<User> userList2 = userDao.getAllUsers();
-        for (User user : userList) {
+        List<User> userList2 = userService.getAllUsers();
+        for (User user : userList2) {
             System.out.println(user);
         }
 
 
-        userDao.cleanUsersTable();
+        userService.cleanUsersTable();
 
 
-        userDao.dropUsersTable();
+        userService.dropUsersTable();
+
+
+        Util.closeConnection();
     }
 }
